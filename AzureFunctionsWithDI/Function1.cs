0a0578fd77780c339abed4ef6f197b74cf2a5e70
Mika Berglund <mika.berglund@meteoriitti.com>
2019-05-09 17:19:32 +0300

    Stripped down the HTTP triggered function to its bare minimum.

--- AzureFunctionsWithDI/Function1.cs
+++ AzureFunctionsWithDI/Function1.cs
@@ -13,21 +13,9 @@ namespace AzureFunctionsWithDI
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req, ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            return new OkResult();
         }
     }
 }
