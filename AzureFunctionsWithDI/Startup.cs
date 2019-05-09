0cffe924cae143dc59c5833e5d3552e5974f209b
Mika Berglund <mika.berglund@meteoriitti.com>
2019-05-09 17:23:05 +0300

    Added necessary package reference to enable DI, and configured a startup class for the application.

---
+++ AzureFunctionsWithDI/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(AzureFunctionsWithDI.Startup))]
+
+namespace AzureFunctionsWithDI
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+        }
+    }
+}
